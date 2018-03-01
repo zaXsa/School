a5101991c066c57319156fab108a2a63eba7c71c
Renzo B <TNCisDench@gmail.com>
2018-03-01 14:58:43 +0100

    It works
    stolen but you know

--- Ch5_exr1/Ch5_exr1/main.cpp
+++ Ch5_exr1/Ch5_exr1/main.cpp
@@ -1,70 +1,43 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-using namespace std;
+typedef struct {
+    int count;
+    char *pText;                                                            /* must point to dynamic allocated memory! */
+} problem_t;
 
-typedef struct{
-    int Numb;
-    char Letter[3];
-}t_PostCode;
+int deepCopy(problem_t *pDestination, const problem_t *pSource);
 
-
-
-int cmp(const void *a, const void *b);
-
-int main(){
-    int i = 0;
-
-    t_PostCode PostCodes[6];
-
-    strcpy(PostCodes[0].Letter, "AB");
-    PostCodes[0].Numb = 1234;
-
-    strcpy(PostCodes[1].Letter, "CC");
-    PostCodes[1].Numb = 6714;
-
-    strcpy(PostCodes[2].Letter, "FA");
-    PostCodes[2].Numb = 4214;
-
-    strcpy(PostCodes[3].Letter, "HJ");
-    PostCodes[3].Numb = 1231;
-
-    strcpy(PostCodes[4].Letter, "AA");
-    PostCodes[4].Numb = 1234;
-
-    strcpy(PostCodes[5].Letter, "AC");
-    PostCodes[5].Numb = 1234;
-
-
-    for(i=0;i<6;i++){
-        cout << PostCodes[i].Numb << " " << PostCodes[i].Letter << endl;
+int main(void){
+    problem_t s1 = {1, NULL};                                               /* do not do ... = {1, ";ABC";} */
+    problem_t s2 = {2, NULL};
+    s1.pText = (char*)malloc(4 * sizeof(char));                             /* array of 4 chars */
+    if (s1.pText != NULL)
+    {
+        strcpy(s1.pText, "111");
+        deepCopy(&s2, &s1);
+        s2.pText[0] = 'A';
+        printf("s1 = %d %s\n", s1.count, s1.pText);
+        printf("s2 = %d %s\n", s2.count, s2.pText);
+        free(s1.pText);
+        s1.pText = NULL;
     }
-
-    cout << endl << endl;
-    qsort(PostCodes, 6, 8, cmp);
-
-    for(i=0;i<6;i++){
-        cout << PostCodes[i].Numb << " " << PostCodes[i].Letter << endl;
-    }
+    s2.pText[1] = 'B';
+    printf("s1 = %d %s\n", s1.count, s1.pText);
+    printf("s2 = %d %s\n", s2.count, s2.pText);
     return 0;
 }
 
-int cmp(const void *a, const void *b){
-    int result;
-
-    const t_PostCode *PostCodeA = (t_PostCode *)a;
-    const t_PostCode *PostCodeB = (t_PostCode *)b;
-
-    result = (PostCodeA->Numb - PostCodeB->Numb);
-    if (result == 0){
-        result = strcmp(PostCodeB->Letter, PostCodeA->Letter);
-        result = -result;
+int deepCopy(problem_t *pDestination, const problem_t *pSource){
+    size_t nchars = strlen(pSource->pText) + 1;                                        // Determens the size of the needed space to get allocated to th pointer
+    pDestination->count = pSource->count;                                              //
+    free(pDestination->pText);                                                         //
+    pDestination->pText = (char*)malloc(nchars);                                       //
+    if (pDestination->pText == NULL)                                                   // Checks for errors
+    {
+        return -1;
     }
-    if (result < 0)
-       return -1;
-    else if (result > 0)
-       return 1;
-    else
-       return 0;
+    strcpy(pDestination->pText, pSource->pText);
+    return 0;
 }
-
